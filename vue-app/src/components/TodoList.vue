695de404133bc074c37899f86396fc80f6ee755e
RenissonSilva <renissonx@gmail.com>
2021-05-30 19:58:45 -0300

    Criando todo list

---
+++ vue-app/src/components/TodoList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="app">
+    <hr>
+    <h2 class="font-black text-green-700 text-xl my-5">Todo List</h2>
+    <div class="justify-items-center mb-5">
+        <input placeholder="Digite uma tarefa" v-model="text" class="input-add" />
+        <button class="btn-green ml-5" v-on:click="addToList(text)">Adicionar Tarefa</button>
+    </div>
+    <ol>
+      <li class="mb-2" v-for="element in list" v-bind:key="element.id">
+        <p class="text-gray-500">{{ element.text }}</p>
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      list: [],
+    };
+  },
+  methods: {
+    addToList(text) {
+        this.list.push({
+            text,
+        });
+    },
+  },
+};
+</script>
+
+<style>
+    .btn-green {
+        @apply py-2 px-4 bg-green-500 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75 w-1/4;
+    }
+    .input-add {
+        @apply focus:ring-green-400 focus:ring-1 focus:outline-none text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-2 pl-10
+    }
+</style>
--- vue-app/src/views/Home.vue
+++ vue-app/src/views/Home.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
     <Name />
     <Presentation />
+    <TodoList />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Name from '@/components/Name.vue'
+import TodoList from '@/components/TodoList.vue'
 import Presentation from '@/components/Presentation.vue'
 
 export default {
@@ -16,6 +17,7 @@ export default {
   components: {
     Name,
     Presentation,
+    TodoList,
   }
 }
 </script>
